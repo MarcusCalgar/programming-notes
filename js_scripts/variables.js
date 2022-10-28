65d448261419e65dfdc487063d06894c45da5da4
MarcusCalgar <paraszthybalazs@gmail.com>
2022-10-28 15:59:37 +0200

    Added example as variables.js

---
+++ js_scripts/variables.js
@@ -0,0 +1,18 @@
+// DECLARING VARIABLES
+
+var catName = "Mew";
+let dogName = "Fido";
+const CAMEL_NAME = "Roger"; // Constants are often capitalized
+
+// LOGGING TO THE CONSOLE
+
+console.log(`My cat's name is ${catName}.`);
+console.log(`My dog's name is ${dogName}.`);
+console.log(`My camel's name is ${CAMEL_NAME}.`);
+
+// CHANGING VARIABLE CONTENT
+
+let girlName = "Alexandra";
+console.log(`My best friend is ${girlName}.`);
+girlName = "Susie";
+console.log(`My best friend is ${girlName}.`);
